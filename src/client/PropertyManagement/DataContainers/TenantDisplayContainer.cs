42dec1aea47c08db6d69a3dca077d77bd5bf4dcb
Robin_tty <muellerobin95@gmail.com>
2020-01-05 23:32:14 +0100

    Add TenantDisplayContainer

---
+++ src/client/PropertyManagement/DataContainers/TenantDisplayContainer.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Text;
+using PropertyManagement.Database.DataModels;
+
+namespace PropertyManagement.DataContainers
+{
+    public class TenantDisplayContainer : G3Tenant
+    {
+        public string Iban { get; set; }
+        public G3Address Address { get; set; }
+
+        public TenantDisplayContainer(G3Tenant tenant) : base(tenant)
+        {
+            Iban = GetIbansForTenant(tenant);
+            Address = tenant.G3Lease.First().Unit.Property.Adress;
+        }
+
+        /// <summary>
+        /// Constructs a string with all IBANs of a tenant
+        /// </summary>
+        /// <param name="tenant">tenant for which to find the associated IBANs</param>
+        /// <returns>String containing all IBANs of the tenant</returns>
+        private string GetIbansForTenant(G3Tenant tenant)
+        {
+            // ReSharper disable once IdentifierTypo
+            var ibans = new StringBuilder();
+            var accounts = tenant.G3BankAccount.ToList();
+
+            ibans.Append($"{accounts.First().Iban}");
+            accounts.Remove(accounts.First());
+            accounts.ForEach(account => ibans.Append($",{account.Iban}"));
+
+            return ibans.ToString();
+        }
+    }
+}
