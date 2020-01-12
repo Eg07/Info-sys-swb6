96396bc2c875be6d1cf64a86848291de93fea68c
Robin_tty <muellerobin95@gmail.com>
2020-01-12 21:56:43 +0100

    Add Transaction Lists

---
+++ src/client/PropertyManagement/DataContainers/PaymentDisplayContainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropertyManagement.DataContainers
+{
+    public class PaymentDisplayContainer
+    {
+
+    }
+}
--- src/client/PropertyManagement/Domain/ViewModels/TransactionsViewModel.cs
+++ src/client/PropertyManagement/Domain/ViewModels/TransactionsViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace PropertyManagement.Domain.ViewModels
+﻿using System.Collections.ObjectModel;
+using PropertyManagement.DataContainers;
+
+namespace PropertyManagement.Domain.ViewModels
 {
     public class TransactionsViewModel : ViewModelBase
     {
+        public ObservableCollection<TenantDisplayContainer> Tenants { get; set; }
+
+        public TransactionsViewModel()
+        {
+
+        }
     }
 }
